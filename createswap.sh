9530b1f033348274c997633eb9f2f34c55c29943
shatll-s <37956983+shatll-s@users.noreply.github.com>
2023-01-07 12:01:51 +0300

    Create createswap.sh

---
+++ createswap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# https://raw.githubusercontent.com/shatll-s/scripts/main/makeswap.sh
+# curl -sSf -L https://raw.githubusercontent.com/shatll-s/scripts/main/makeswap.sh | sudo sh -s 16G
+[ ! -z $1 ] && size=$1 || size="8G"
+
+echo "Make swap with size $size"
+
+sudo -i
+fallocate -l 8G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile    none    swap    sw    0    0" >> /etc/fstab
+echo "Ok!"
