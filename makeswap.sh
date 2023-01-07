d4da955008621a8095c02964ac3235ba620be09b
shatll-s <37956983+shatll-s@users.noreply.github.com>
2023-01-07 11:59:36 +0300

    Update makeswap.sh

--- makeswap.sh
+++ makeswap.sh
@@ -1,4 +1,10 @@
+#!/bin/sh
+
+# https://raw.githubusercontent.com/shatll-s/scripts/main/makeswap.sh
+# curl -sSf -L https://raw.githubusercontent.com/shatll-s/scripts/main/makeswap.sh | sudo sh -s 16G
 [[ ! -z $1 ]] && size=$1 || size="8G"
+
+echo "Make swap: $size"
 
 sudo -i
 fallocate -l 8G /swapfile
